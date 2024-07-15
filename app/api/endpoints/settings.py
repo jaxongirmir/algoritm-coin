0479d2a120ff3cfce0eeee37d0243c2e46f11003
sanjar-x <sanjar68x@gmail.com>
2024-07-15 13:33:16 +0500

    Remove test router

--- app/api/endpoints/settings.py
+++ app/api/endpoints/settings.py
@@ -19,16 +19,3 @@ async def create_teacher():
         await connection.exec_driver_sql("CREATE SCHEMA public")
         await connection.run_sync(Base.metadata.create_all)
     return {"message": "Database schema was successfully reset."}
-
-
-class LicenseRequest(BaseModel):
-    kids: list
-    type: str
-
-
-@settings_router.post("/proxy/clearkey")
-async def proxy_clearkey(request: LicenseRequest):
-    url = "https://license.kinescope.io/v1/vod/fe1f558e-1043-4f47-9893-e6c924e40f79/acquire/clearkey?token="
-    headers = {"Content-Type": "application/json"}
-    response = requests.post(url, json=request.dict(), headers=headers)
-    return JSONResponse(content=response.json())
